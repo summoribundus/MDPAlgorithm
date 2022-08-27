26172eea64a3c784fd2ee762281549cd59101dff
skye.li <pully@outlook.com>
2022-08-27 17:55:55 +0800

    Abstract out Trajectory to grid

--- src/main/java/ntu/mdp/pathfinding/Algo/ShortestPathAlgo.java
+++ src/main/java/ntu/mdp/pathfinding/Algo/ShortestPathAlgo.java
@@ -8,16 +8,12 @@ import java.util.Map;
 
 public class ShortestPathAlgo {
     private Arena arena;
-    private static int[] quadrantReverse = new int[]{0, 3, 4, 1, 2};
-    private static int[] quadrantSameYSide = new int[]{0, 4, 3, 2, 1};
-    private static int[] quadrantSameXSide = new int[]{0, 2, 1, 4, 3};
-
-    private int R;
-
     private Obstacle[] obstacles;
     private int carR, carC;
-
     private ShortestPathBF shortestPathBF;
+    private List<int[]> lastPathGrids;
+
+    private List<CarMove> moveSteps;
 
     public ShortestPathAlgo(int m, int n, Obstacle[] obstacles, int r, int c) {
         this.obstacles = obstacles;
@@ -32,9 +28,10 @@ public class ShortestPathAlgo {
 
         Map<Integer, Obstacle> idxMapping = shortestPathBF.getIdxMapping();
         while (shortestPathBF.hasNextPath()) {
+            lastPathGrids = new ArrayList<>();
+            moveSteps = new ArrayList<>();
             int[] path = shortestPathBF.getNextPath();
             boolean pathValid = true;
-            ArrayList<CarMove> moveSteps = new ArrayList<>();
             Obstacle car = idxMapping.get(path[0]);
             int carR = car.getR(), carC = car.getC(), theta = 90;
             for (int i = 1; i < path.length-1; i++) {
@@ -55,74 +52,35 @@ public class ShortestPathAlgo {
         int r2 = trajectoryResult.getPt2()[0], c2 = trajectoryResult.getPt2()[1];
         int circle1R = trajectoryResult.getCircle1()[0], circle1C = trajectoryResult.getCircle1()[1];
         int circle2R = trajectoryResult.getCircle2()[0], circle2C = trajectoryResult.getCircle2()[1];
-        return checkCircle(startR, startC, r1, c1, circle1R, circle1C) && checkLine(r1, c1, r2, c2) &&
-                checkCircle(r2, c2, endR, endC, circle2R, circle2C);
+        boolean isClockwise1 = trajectoryResult.isClockwiseTurn1(), isClockwise2 = trajectoryResult.isClockwiseTurn2();
+
+        List<int[]> points = TrajectoryToArenaGrid.findGridCirclePath(startR, startC, r1, c1, circle1R, circle1C, isClockwise1);
+        if (!validatePath(points)) return false;
+        lastPathGrids.addAll(points);
+
+        points = TrajectoryToArenaGrid.findGirdLinePath(r1, c1, r2, c2);
+        if (!validatePath(points)) return false;
+        lastPathGrids.addAll(points);
+
+        points = TrajectoryToArenaGrid.findGridCirclePath(r2, c2, endR, endC, circle2R, circle2C, isClockwise2);
+        if(!validatePath(points)) return false;
+        lastPathGrids.addAll(points);
+
+        return true;
     }
 
-    private boolean checkLine(int r1, int c1, int r2, int c2) {
-        double a = (double) (r2 - r1) / (c2 - c1), b = r1 - c1 * a;
-        for (int i = c1; i <= c2; i++) {
-            double y = a * i + b;
-            int j = (int) Math.ceil(y);
-            if (!arena.checkWithCorrespondingBlock(i, j)) return false;
+    private boolean validatePath(List<int[]> points) {
+        for (int[] p : points) {
+            if (arena.checkWithCorrespondingBlock(p[0], p[1])) return false;
         }
         return true;
     }
 
-    private boolean checkCircle(int r1, int c1, int r2, int c2, int circleR, int circleC) {
-        int recTanR1 = 0, recTanC1 = 0, recTanR2 = 0, recTanC2 = 0;
-        int quadrant1 = getQuadrantOfPoints(r1, c1, circleR, circleC);
-        int quadrant2 = getQuadrantOfPoints(r2, c2, circleR, circleC);
-
-        if (quadrant1 == quadrant2) {
-            recTanR1 = Math.min(r1, r2); recTanR2 = Math.max(r1, r2);
-            recTanC1 = Math.min(c1, c2); recTanC2 = Math.max(c1, c2);
-        } else if (checkReverseQuadrant(quadrant1, quadrant2)) {
-            recTanR1 = circleR - R; recTanR2 = circleR + R;
-            recTanC1 = circleC - R; recTanC2 = circleC + R;
-        } else if (checkQuadrantSameXSide(quadrant1, quadrant2)) {
-            recTanR1 = quadrant1 <= 2 ? circleR - R : Math.min(r1, r2);
-            recTanR2 = quadrant1 <= 2 ? Math.max(r1, r2) : circleR + R;
-            recTanC1 = Math.min(c1, c2); recTanC2 = Math.max(c1, c2);
-        } else if(checkQuadrantSameYSide(quadrant1, quadrant2)) {
-            recTanR1 = Math.min(r1, r2); recTanR2 = Math.max(r1, r2);
-            recTanC1 = (quadrant1 == 2 || quadrant1 == 3) ? circleC - R : Math.min(c1, c2);
-            recTanC2 = (quadrant1 == 2 || quadrant1 == 3) ? Math.max(c1, c2) : circleC + R;
-        }
-
-        return checkPathWithArena(recTanR1, recTanC1, recTanR2, recTanC2);
+    public List<int[]> getLastPathGrids() {
+        return lastPathGrids;
     }
 
-    private boolean checkPathWithArena(int r1, int c1, int r2, int c2) {
-        for (int i  = r1; i <= r2; i++) {
-            if (!arena.checkWithCorrespondingBlock(i, c1)) return false;
-            if (!arena.checkWithCorrespondingBlock(i, c2)) return false;
-        }
-
-        for (int j = c1; j <= c2; j++) {
-            if (!arena.checkWithCorrespondingBlock(r1, j)) return false;
-            if (!arena.checkWithCorrespondingBlock(r2, j)) return false;
-        }
-        return true;
-    }
-
-    private int getQuadrantOfPoints(int r, int c, int circleR, int circleC) {
-        if (c >= circleC) {
-            return r <= circleR ? 1 : 4;
-        } else {
-            return r <= circleR ? 2 : 3;
-        }
-    }
-
-    private boolean checkReverseQuadrant(int quadrant1, int quadrant2) {
-        return quadrantReverse[quadrant1] == quadrant2;
-    }
-
-    private boolean checkQuadrantSameXSide(int quadrant1, int quadrant2) {
-        return quadrantSameXSide[quadrant1] == quadrant2;
-    }
-
-    private boolean checkQuadrantSameYSide(int quadrant1, int quadrant2) {
-        return quadrantSameYSide[quadrant1] == quadrant2;
+    public List<CarMove> getMoveSteps() {
+        return moveSteps;
     }
 }
--- src/main/java/ntu/mdp/pathfinding/Algo/TrajectoryResult.java
+++ src/main/java/ntu/mdp/pathfinding/Algo/TrajectoryResult.java
@@ -3,15 +3,17 @@ package ntu.mdp.pathfinding.Algo;
 public class TrajectoryResult {
     private int[] pt1, pt2, circle1, circle2;
 
-    private boolean isClockwiseTurn;
+    private boolean isClockwiseTurn1, isClockwiseTurn2;
     private CarMove carMove;
 
-    public TrajectoryResult(int[] pt1, int[] pt2, int[] circle1, int[] circle2, int theta1, int theta2, int length) {
+    public TrajectoryResult(int[] pt1, int[] pt2, int[] circle1, int[] circle2, int theta1, int theta2, int length, boolean isClockwiseTurn1, boolean isClockwiseTurn2) {
         this.pt1 = pt1;
         this.pt2 = pt2;
         this.circle1 = circle1;
         this.circle2 = circle2;
         this.carMove = new CarMove(theta1, theta2, length);
+        this.isClockwiseTurn1 = isClockwiseTurn1;
+        this.isClockwiseTurn2 = isClockwiseTurn2;
     }
 
     public int[] getPt1() {
@@ -32,5 +34,13 @@ public class TrajectoryResult {
 
     public CarMove getCarMove() {
         return carMove;
+    }
+
+    public boolean isClockwiseTurn1() {
+        return isClockwiseTurn1;
+    }
+
+    public boolean isClockwiseTurn2() {
+        return isClockwiseTurn2;
     }
 }
