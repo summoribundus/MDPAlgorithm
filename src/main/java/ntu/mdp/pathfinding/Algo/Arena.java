e5359eb2dc4857f25a39c8393dd2f0a9c48a8168
skye.li <pully@outlook.com>
2022-09-07 12:29:12 +0800

    add validate path function to arena

--- src/main/java/ntu/mdp/pathfinding/Algo/Arena.java
+++ src/main/java/ntu/mdp/pathfinding/Algo/Arena.java
@@ -2,6 +2,8 @@ package ntu.mdp.pathfinding.Algo;
 
 import ntu.mdp.pathfinding.Obstacle;
 
+import java.util.List;
+
 public class Arena {
     private Node[][] arena;
 
@@ -41,6 +43,13 @@ public class Arena {
                 if (inRange(nr, nc))
                     arena[nr][nc].setVirtualBlock(true);
             }
+    }
+
+    public boolean validatePoint(List<int[]> points) {
+        for (int[] p : points) {
+            if (checkWithCorrespondingBlock(p[0], p[1])) return false;
+        }
+        return true;
     }
 
     // 5 based r and c
