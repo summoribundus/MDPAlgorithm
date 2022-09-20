67f65d3f678882cbac9377c034c1a5df4ae42b00
skye.li <pully@outlook.com>
2022-09-20 15:52:05 +0800

    set up receive info from android

--- src/main/java/ntu/mdp/pathfinding/RPIApplication.java
+++ src/main/java/ntu/mdp/pathfinding/RPIApplication.java
@@ -18,13 +18,30 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class RPIApplication {
-    private final static String IP = "localhost";
-    private final static int port = 2223;
+    private final static String RPI = "localhost";
+    private final static int RPI_PORT = 2223;
+    private final static String HOST = "10.27.182.66";
+    private final static int Host_PORT = 2223;
     private static DatagramSocket socket;
 
     public static void main(String[] args) throws IOException {
-        socket = new DatagramSocket();
+        DatagramSocket serverSocket = new DatagramSocket(Host_PORT);
+        byte[] obstacleBuf = new byte[128];
+        DatagramPacket packet = new DatagramPacket(obstacleBuf, obstacleBuf.length);
+        serverSocket.receive(packet);
+        serverSocket.close();
 
+        String boardConfigStr = new String(packet.getData(), 0, packet.getLength());
+        String[] boardConfigStrSplit = boardConfigStr.split(",");
+        Obstacle[] obstacles = constructObstacleFromString(boardConfigStrSplit[1]);
+        String[] carConfig = boardConfigStrSplit[0].split(":");
+
+        List<CarMove> carMoves = findShortestPath(obstacles, Integer.parseInt(carConfig[1]), Integer.parseInt(carConfig[0]));
+        if (carMoves == null) {
+            System.out.println("No solution found. Not making movement");
+            closeConnection();
+            return;
+        }
 //        String message = "Hello";
 //        byte[] buf = message.getBytes(StandardCharsets.UTF_8);
 //        byte[] buffer = new byte[512];
@@ -34,14 +51,8 @@ public class RPIApplication {
 //        socket.receive(packet);
 //        System.out.println(new String(packet.getData()));
 
+        socket = new DatagramSocket();
         byte[] buffer = new byte[256];
-
-        List<CarMove> carMoves = findShortestPath();
-        if (carMoves == null) {
-            System.out.println("No solution found. Not making movement");
-            closeConnection();
-            return;
-        }
 
         long startTime = System.currentTimeMillis();
         while (true) {
@@ -63,12 +74,25 @@ public class RPIApplication {
 
     private static void sendCommand(String command) throws IOException {
         byte[] buf = command.getBytes(StandardCharsets.UTF_8);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(IP), port);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName(RPI), RPI_PORT);
         socket.send(packet);
     }
-    private static List<CarMove> findShortestPath() {
-        ShortestPathBF shortestPathBF = new ShortestPathBF(InputData.getObstacles(), InputData.getStartR(), InputData.getStartC());
-        Arena arena = new Arena(AlgoConstant.GridM, AlgoConstant.GridN, InputData.getObstacles());
+
+    private static Obstacle[] constructObstacleFromString(String obstacleStrs) {
+        String[] obstacleStrsSplit = obstacleStrs.split(";");
+        Obstacle[] obstacles = new Obstacle[obstacleStrs.length()];
+        int cnt = 0;
+        for (String obstacleStr : obstacleStrsSplit) {
+            String[] obstacleIdx = obstacleStr.split(":");
+            obstacles[cnt++] = new Obstacle(Integer.parseInt(obstacleIdx[1]) * 2,
+                    Integer.parseInt(obstacleIdx[0]) * 2,
+                    Integer.parseInt(obstacleIdx[2]) * 2, 1, false);
+        }
+        return obstacles;
+    }
+    private static List<CarMove> findShortestPath(Obstacle[] obstacles, int r, int c) {
+        ShortestPathBF shortestPathBF = new ShortestPathBF(obstacles, r, c);
+        Arena arena = new Arena(AlgoConstant.GridM, AlgoConstant.GridN, obstacles);
         ShortestPathTrajectoryAlgo algo = new ShortestPathTrajectoryAlgo(arena, shortestPathBF);
         ShortestPathTrajectoryResult result = algo.findShortestPath();
         if (result != null) {
