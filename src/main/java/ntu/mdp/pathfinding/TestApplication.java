e9320564b919a063857c625de1e38f316bf25669
skye.li <pully@outlook.com>
2022-09-26 17:11:51 +0800

    for rpi testing

--- src/main/java/ntu/mdp/pathfinding/TestApplication.java
+++ src/main/java/ntu/mdp/pathfinding/TestApplication.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class TestApplication {
     private final static String RPI = "192.168.17.17";
@@ -14,9 +15,14 @@ public class TestApplication {
         Socket socket1 = new Socket(RPI, RPI_PORT);
         PrintWriter out = new PrintWriter(socket1.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
+        Scanner sc = new Scanner(System.in);
 
-        out.println("STM:01050");
-        System.out.println("Pending read");
-        System.out.println(in.readLine());
+        while (true) {
+            out.println("STM:01010");
+            System.out.println("Pending read");
+            System.out.println(in.readLine());
+            //sc.nextLine();
+        }
+
     }
 }
